ffa943cb4950b654b3cb8bcfabfbc1575a2b341c
gilberto.brito <gilberto.brito@gha-09.tacom.eng.pro>
2022-03-21 14:46:04 -0300

    Iniciando o imc

---
+++ DevSuperior/src/br/com/funcoes/git/imc.java
@@ -0,0 +1,20 @@
+package br.com.funcoes.git;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class imc {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
